80fbbd7ab0c72be7196329ef0c3544b49ea08624
Lorenzo Pieri <email@lorenzopieri.dev>
2020-11-06 15:09:32 +0100

    chore: release 0.2.0

--- .release-it.js
+++ .release-it.js
@@ -2,12 +2,9 @@ module.exports = {
 	git: {
 		commitMessage: 'chore: release ${version}',
 		requireBranch: 'main',
-		requireCleanWorkingDir: true,
+		requireCleanWorkingDir: false,
 		// requireCommits: true,
 		tagName: 'v${version}',
-	},
-	github: {
-		release: true,
 	},
 	npm: {
 		publish: true,
